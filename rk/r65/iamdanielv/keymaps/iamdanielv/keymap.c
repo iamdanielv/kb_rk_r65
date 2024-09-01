753cc6b1709ece8dfef3408dcfe0b09bbcb2cc4f
Daniel <1767914+iamdanielv@users.noreply.github.com>
2024-08-31 23:23:04 -0700

    typo

--- rk/r65/iamdanielv/keymaps/iamdanielv/keymap.c
+++ rk/r65/iamdanielv/keymaps/iamdanielv/keymap.c
@@ -579,7 +579,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         }
     }
 
-    processIndicatorQueue();
+    process_indicator_queue(led_min, led_max);
 
     return true;
 }
