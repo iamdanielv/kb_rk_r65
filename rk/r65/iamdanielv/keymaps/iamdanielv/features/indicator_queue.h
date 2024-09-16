b11b861d437162e384274c87b6d32ca6bcbcbe71
Daniel <1767914+iamdanielv@users.noreply.github.com>
2024-09-15 17:18:05 -0700

    lint

--- rk/r65/iamdanielv/keymaps/iamdanielv/features/indicator_queue.h
+++ rk/r65/iamdanielv/keymaps/iamdanielv/features/indicator_queue.h
@@ -11,31 +11,32 @@
 
 // this is similar to RGB_MATRIX_INDICATOR_SET_COLOR
 // but takes in an indicator_t instead
-#define INDICATOR_Q_MATRIX_SET_COLOR(indicator)    \
-   if (indicator.led_index >= led_min && indicator.led_index < led_max) {               \
-       rgb_matrix_set_color(indicator.led_index, indicator.r, indicator.g, indicator.b);\
-   }
+#define INDICATOR_Q_MATRIX_SET_COLOR(indicator)                                           \
+    if (indicator.led_index >= led_min && indicator.led_index < led_max) {                \
+        rgb_matrix_set_color(indicator.led_index, indicator.r, indicator.g, indicator.b); \
+    }
 
-#define INDICATOR_Q_MATRIX_SET_COLOR_CUSTOM(indicator, r, g, b)    \
-   if (indicator.led_index >= led_min && indicator.led_index < led_max) {               \
-       rgb_matrix_set_color(indicator.led_index, r, g, b);\
-   }
+#define INDICATOR_Q_MATRIX_SET_COLOR_CUSTOM(indicator, r, g, b)            \
+    if (indicator.led_index >= led_min && indicator.led_index < led_max) { \
+        rgb_matrix_set_color(indicator.led_index, r, g, b);                \
+    }
 
-#define INDICATOR_Q_GET_RGB_LED(indicator) \
-   (rgb_led_t) { .r = indicator.r, .g = indicator.g, .b = indicator.b }
-
+#define INDICATOR_Q_GET_RGB_LED(indicator)                   \
+    (rgb_led_t) {                                            \
+        .r = indicator.r, .g = indicator.g, .b = indicator.b \
+    }
 
 #define INDICATOR_QUEUE_MAX 20
 
 typedef struct {
-    bool active;
-    uint8_t led_index;
+    bool     active;
+    uint8_t  led_index;
     uint32_t last_update;
     uint32_t interval;
-    uint8_t times_to_flash;
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
+    uint8_t  times_to_flash;
+    uint8_t  r;
+    uint8_t  g;
+    uint8_t  b;
 } indicator_t;
 
 indicator_t indicator_queue[INDICATOR_QUEUE_MAX];
