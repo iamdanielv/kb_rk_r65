6be439772b498fd3094b2faee8b20bbe7073cc72
Daniel <1767914+iamdanielv@users.noreply.github.com>
2024-09-15 17:16:36 -0700

    lint

--- rk/r65/iamdanielv/keymaps/iamdanielv/features/indicator_queue.c
+++ rk/r65/iamdanielv/keymaps/iamdanielv/features/indicator_queue.c
@@ -5,28 +5,28 @@ rgb_led_t get_complementary_color(rgb_led_t rgb_led, bool darken) {
     uint8_t new_g = 0xFF - rgb_led.g;
     uint8_t new_b = 0xFF - rgb_led.b;
 
-    if(darken) {
+    if (darken) {
         // darken the new color by shifting all values down
-        if( new_r > 0x80) { new_r = new_r - 0x80;}
-        if( new_g > 0x80) { new_g = new_g - 0x80;}
-        if( new_b > 0x80) { new_b = new_b - 0x80;}
+        if (new_r > 0x80) { new_r = new_r - 0x80; }
+        if (new_g > 0x80) { new_g = new_g - 0x80; }
+        if (new_b > 0x80) { new_b = new_b - 0x80; }
     }
 
-    return (rgb_led_t){.r=new_r, .g = new_g, .b = new_b};
+    return (rgb_led_t){.r = new_r, .g = new_g, .b = new_b};
 }
 
 void indicator_enqueue(uint8_t led_index, uint32_t interval, uint8_t times_to_flash, uint8_t r, uint8_t g, uint8_t b) {
     for (int i = 0; i < INDICATOR_QUEUE_MAX; i++) {
         if (!indicator_queue[i].active) {
             // this queue position is not active, so we can use it
-            indicator_queue[i].active = true;
-            indicator_queue[i].led_index = led_index;
-            indicator_queue[i].last_update = timer_read32();
-            indicator_queue[i].interval = interval;
+            indicator_queue[i].active         = true;
+            indicator_queue[i].led_index      = led_index;
+            indicator_queue[i].last_update    = timer_read32();
+            indicator_queue[i].interval       = interval;
             indicator_queue[i].times_to_flash = times_to_flash * 2;
-            indicator_queue[i].r = r;
-            indicator_queue[i].g = g;
-            indicator_queue[i].b = b;
+            indicator_queue[i].r              = r;
+            indicator_queue[i].g              = g;
+            indicator_queue[i].b              = b;
             break;
         }
     }
@@ -48,7 +48,7 @@ void process_indicator_queue(uint8_t led_min, uint8_t led_max) {
                 if (indicator_queue[i].times_to_flash <= 0) {
                     // we have flashed as many times as requested
                     // clear this queue spot
-                    indicator_queue[i].active = false;
+                    indicator_queue[i].active      = false;
                     indicator_queue[i].last_update = 0x00;
                 }
             }
@@ -56,8 +56,8 @@ void process_indicator_queue(uint8_t led_min, uint8_t led_max) {
             if (indicator_queue[i].times_to_flash % 2) {
                 INDICATOR_Q_MATRIX_SET_COLOR(indicator_queue[i]);
             } else {
-                rgb_led_t this_rgb_led =  INDICATOR_Q_GET_RGB_LED(indicator_queue[i]);
-                rgb_led_t alt = get_complementary_color(this_rgb_led, false);
+                rgb_led_t this_rgb_led = INDICATOR_Q_GET_RGB_LED(indicator_queue[i]);
+                rgb_led_t alt          = get_complementary_color(this_rgb_led, false);
                 INDICATOR_Q_MATRIX_SET_COLOR_CUSTOM(indicator_queue[i], alt.r, alt.g, alt.b);
             }
         }
