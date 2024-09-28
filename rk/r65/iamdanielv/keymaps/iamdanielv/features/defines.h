021fcc81a0a3c2c99f01fc35c4588d03e1d6466a
Daniel <1767914+iamdanielv@users.noreply.github.com>
2024-09-28 09:27:25 -0700

    move defines
    keymap is getting bigger, split up the code

---
+++ rk/r65/iamdanielv/keymaps/iamdanielv/features/defines.h
@@ -0,0 +1,34 @@
+#pragma once
+
+enum layer_names {
+    _WIN_LYR,    // 0
+    _WIN_FN_LYR, // 1
+    _CTL_LYR,    // 2
+    _NUM_LYR,    // 3
+    _NAV_LYR,    // 4
+    _FN_LYR,     // 5
+};
+
+// ******************************
+// * Aliases to simplify keymap *
+// ******************************
+#define W_ALT_CAPS LT(_WIN_FN_LYR, KC_CAPS)
+#define FN_RALT LT(_FN_LYR, KC_RALT)
+
+#define MY_UNDO   C(KC_Z)
+#define MY_CUT    C(KC_X)
+#define MY_COPY   C(KC_INS)
+#define MY_PASTE  S(KC_INS)
+#define MY_TASK   LCTL(LSFT(KC_ESC))
+#define MY_CONS   LCTL(LSFT(KC_GRV))
+#define MY_BACK   A(KC_LEFT)
+#define MY_FWD    A(KC_RIGHT)
+
+#define TG_NAV    TG(_NAV_LYR)
+#define TG_NUM    TG(_NUM_LYR)
+#define TG_CTL    TG(_CTL_LYR)
+#define MO_CTL    MO(_CTL_LYR)
+#define MO_FN     MO(_WIN_FN_LYR)
+#define TD_TG_CTL TD(TD_CTL_TG)
+#define TD_KB_RST TD(TD_RESET)
+#define TD_KB_CLR TD(TD_CLEAR)
--- rk/r65/iamdanielv/keymaps/iamdanielv/keymap.c
+++ rk/r65/iamdanielv/keymaps/iamdanielv/keymap.c
@@ -3,17 +3,10 @@
 
 #include QMK_KEYBOARD_H
 
+#include "features/defines.h"
 #include "features/indicator_queue.h"
 #include "features/fn_mode.h"
 
-enum layer_names {
-    _WIN_LYR,    // 0
-    _WIN_FN_LYR, // 1
-    _CTL_LYR,    // 2
-    _NUM_LYR,    // 3
-    _NAV_LYR,    // 4
-    _FN_LYR,     // 5
-};
 
 void blink_arrows(void);
 void blink_space(bool);
@@ -297,29 +290,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-// ******************************
-// * Aliases to simplify keymap *
-// ******************************
-#define W_ALT_CAPS LT(_WIN_FN_LYR, KC_CAPS)
-#define FN_RALT LT(_FN_LYR, KC_RALT)
-
-#define MY_UNDO   C(KC_Z)
-#define MY_CUT    C(KC_X)
-#define MY_COPY   C(KC_INS)
-#define MY_PASTE  S(KC_INS)
-#define MY_TASK   LCTL(LSFT(KC_ESC))
-#define MY_CONS   LCTL(LSFT(KC_GRV))
-#define MY_BACK   A(KC_LEFT)
-#define MY_FWD    A(KC_RIGHT)
-
-#define TG_NAV    TG(_NAV_LYR)
-#define TG_NUM    TG(_NUM_LYR)
-#define TG_CTL    TG(_CTL_LYR)
-#define MO_CTL    MO(_CTL_LYR)
-#define MO_FN     MO(_WIN_FN_LYR)
-#define TD_TG_CTL TD(TD_CTL_TG)
-#define TD_KB_RST TD(TD_RESET)
-#define TD_KB_CLR TD(TD_CLEAR)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
