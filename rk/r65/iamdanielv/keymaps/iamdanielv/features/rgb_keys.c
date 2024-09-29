4c3629e348aa06390ded34b1d6ce6a4a839a40f0
Daniel <1767914+iamdanielv@users.noreply.github.com>
2024-09-28 17:06:49 -0700

    BUG: RGB keys
    when moved to stand alone file removed check for only pressed events. Caused bug that RGB keys were being handled for releases when they should have just been ignored.
    
    Added blink on HSV modification to show that something is being registered

--- rk/r65/iamdanielv/keymaps/iamdanielv/features/rgb_keys.c
+++ rk/r65/iamdanielv/keymaps/iamdanielv/features/rgb_keys.c
@@ -5,114 +5,145 @@
 #include "rgb_keys.h"
 #include "indicator_queue.h"
 
-
-bool process_rgb_keys(uint16_t keycode, keyrecord_t *record){
-
-    // we only care about pressed events, return as soon as possible
-    if (!record->event.pressed) { return true;}
-
+bool process_rgb_keys(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case RGB_TOG:
-            switch (rgb_matrix_get_flags()) {
-                case LED_FLAG_ALL: {
-                    rgb_matrix_set_flags_noeeprom(LED_FLAG_INDICATOR);
-                    // rgb_matrix_set_color_all(0, 0, 0);
-                    indicator_enqueue(65, 200, 3, INDICATOR_RGB_DARK_RED); // blink space too
-                    indicator_enqueue(0, 200, 3, INDICATOR_RGB_DARK_RED);  // blink left alt
-                    indicator_enqueue(64, 200, 3, INDICATOR_RGB_DARK_RED); // blink right alt
-                } break;
-                default: {
-                    HSV current_hsv = rgb_matrix_get_hsv();
-                    RGB rgb         = hsv_to_rgb(current_hsv);
-                    rgb_matrix_set_color_all(rgb.r, rgb.g, rgb.b);
-                    rgb_matrix_set_flags_noeeprom(LED_FLAG_ALL);
-                    blink_space(true);
-                } break;
+            if (record->event.pressed) {
+                switch (rgb_matrix_get_flags()) {
+                    case LED_FLAG_ALL: {
+                        rgb_matrix_set_flags_noeeprom(LED_FLAG_INDICATOR);
+                        // rgb_matrix_set_color_all(0, 0, 0);
+                        indicator_enqueue(65, 200, 3, INDICATOR_RGB_DARK_RED); // blink space too
+                        indicator_enqueue(0, 200, 3, INDICATOR_RGB_DARK_RED);  // blink left alt
+                        indicator_enqueue(64, 200, 3, INDICATOR_RGB_DARK_RED); // blink right alt
+                    } break;
+                    default: {
+                        HSV current_hsv = rgb_matrix_get_hsv();
+                        RGB rgb         = hsv_to_rgb(current_hsv);
+                        rgb_matrix_set_color_all(rgb.r, rgb.g, rgb.b);
+                        rgb_matrix_set_flags_noeeprom(LED_FLAG_ALL);
+                        blink_space(true);
+                    } break;
+                }
             }
             return false;
         case RGB_MOD:
-            rgb_matrix_step_noeeprom();
+            if (record->event.pressed) {
+                rgb_matrix_step_noeeprom();
+            }
             return false;
         case RGB_RMOD:
-            rgb_matrix_step_reverse_noeeprom();
+            if (record->event.pressed) {
+                rgb_matrix_step_reverse_noeeprom();
+            }
             return false;
         case RGB_M_P:
-            rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
-            blink_space(true);
+            if (record->event.pressed) {
+                rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+                blink_space(true);
+            }
             return false;
         case RGB_SPI:
-            if (rgb_matrix_get_speed() >= (255 - RGB_MATRIX_SPD_STEP)) {
-                // this update would put us at max
-                blink_arrows();
-                indicator_enqueue(17, 200, 4, RGB_RED);   // ' - SPI
-                indicator_enqueue(18, 200, 2, RGB_BLACK); // ; - SPD
-                blink_space(false);
+            if (record->event.pressed) {
+                if (rgb_matrix_get_speed() >= (255 - RGB_MATRIX_SPD_STEP)) {
+                    // this update would put us at max
+                    blink_arrows();
+                    indicator_enqueue(17, 200, 4, INDICATOR_RGB_DARK_RED); // ' - SPI
+                    indicator_enqueue(18, 200, 2, RGB_BLACK);              // ; - SPD
+                    blink_space(false);
+                }
+                rgb_matrix_increase_speed_noeeprom();
             }
-            rgb_matrix_increase_speed_noeeprom();
             return false;
         case RGB_SPD:
-            if (rgb_matrix_get_speed() <= RGB_MATRIX_SPD_STEP) {
-                blink_arrows();
-                indicator_enqueue(17, 200, 2, RGB_BLACK); // ' - SPI
-                indicator_enqueue(18, 200, 4, RGB_RED);   // ; - SPD
-                blink_space(false);
-                rgb_matrix_set_speed_noeeprom(RGB_MATRIX_SPD_STEP);
+            if (record->event.pressed) {
+                if (rgb_matrix_get_speed() <= RGB_MATRIX_SPD_STEP) {
+                    blink_arrows();
+                    indicator_enqueue(17, 200, 2, RGB_BLACK);              // ' - SPI
+                    indicator_enqueue(18, 200, 4, INDICATOR_RGB_DARK_RED); // ; - SPD
+                    blink_space(false);
+                    rgb_matrix_set_speed_noeeprom(RGB_MATRIX_SPD_STEP);
+                }
+                rgb_matrix_decrease_speed_noeeprom();
             }
-            rgb_matrix_decrease_speed_noeeprom();
             return false;
         case RGB_HUI:
-            if (rgb_matrix_get_hue() >= (255 - RGB_MATRIX_HUE_STEP)) {
-                // this update would put us at max
-                indicator_enqueue(38, 200, 4, INDICATOR_RGB_DARK_RED); // O - HUI
-                indicator_enqueue(37, 200, 2, RGB_BLACK);              // I - HUD
-                blink_space(false);
+            if (record->event.pressed) {
+                if (rgb_matrix_get_hue() >= (255 - RGB_MATRIX_HUE_STEP)) {
+                    // this update would put us at max
+                    indicator_enqueue(38, 200, 4, INDICATOR_RGB_DARK_RED); // O - HUI
+                    indicator_enqueue(37, 200, 2, RGB_BLACK);              // I - HUD
+                    blink_space(false);
+                } else {
+                    indicator_enqueue(38, 150, 1, RGB_WHITE); // O - HUI
+                }
+                rgb_matrix_increase_hue_noeeprom();
             }
-            rgb_matrix_increase_hue_noeeprom();
             return false;
         case RGB_HUD:
-            if (rgb_matrix_get_hue() <= RGB_MATRIX_HUE_STEP) {
-                // this update would put us at min
-                indicator_enqueue(38, 200, 2, RGB_BLACK);              // O - HUI
-                indicator_enqueue(37, 200, 4, INDICATOR_RGB_DARK_RED); // I - HUD
-                blink_space(false);
+            if (record->event.pressed) {
+                if (rgb_matrix_get_hue() <= RGB_MATRIX_HUE_STEP) {
+                    // this update would put us at min
+                    indicator_enqueue(38, 200, 2, RGB_BLACK);              // O - HUI
+                    indicator_enqueue(37, 200, 4, INDICATOR_RGB_DARK_RED); // I - HUD
+                    blink_space(false);
+                } else {
+                    indicator_enqueue(37, 150, 1, RGB_WHITE); // I - HUD
+                }
+                rgb_matrix_decrease_hue_noeeprom();
             }
-            rgb_matrix_decrease_hue_noeeprom();
             return false;
         case RGB_SAI:
-            if (rgb_matrix_get_sat() >= (255 - RGB_MATRIX_SAT_STEP)) {
-                // this update would put us at max
-                indicator_enqueue(19, 200, 4, INDICATOR_RGB_DARK_RED); // L - SAI
-                indicator_enqueue(20, 200, 2, RGB_BLACK);              // K - SAD
-                blink_space(false);
+            if (record->event.pressed) {
+                if (rgb_matrix_get_sat() >= (255 - RGB_MATRIX_SAT_STEP)) {
+                    // this update would put us at max
+                    indicator_enqueue(19, 200, 4, INDICATOR_RGB_DARK_RED); // L - SAI
+                    indicator_enqueue(20, 200, 2, RGB_BLACK);              // K - SAD
+                    blink_space(false);
+                }
+                rgb_matrix_increase_sat_noeeprom();
+            } else {
+                indicator_enqueue(19, 150, 1, RGB_WHITE); // L - SAI
             }
-            rgb_matrix_increase_sat_noeeprom();
             return false;
         case RGB_SAD:
-            if (rgb_matrix_get_sat() <= RGB_MATRIX_SAT_STEP) {
-                // this update would put us at min
-                indicator_enqueue(19, 200, 2, RGB_BLACK);              // L - SAI
-                indicator_enqueue(20, 200, 4, INDICATOR_RGB_DARK_RED); // K - SAD
-                blink_space(false);
+            if (record->event.pressed) {
+                if (rgb_matrix_get_sat() <= RGB_MATRIX_SAT_STEP) {
+                    // this update would put us at min
+                    indicator_enqueue(19, 200, 2, RGB_BLACK);              // L - SAI
+                    indicator_enqueue(20, 200, 4, INDICATOR_RGB_DARK_RED); // K - SAD
+                    blink_space(false);
+                }
+                rgb_matrix_decrease_sat_noeeprom();
+            } else {
+                indicator_enqueue(20, 150, 1, RGB_WHITE); // K - SAD
             }
-            rgb_matrix_decrease_sat_noeeprom();
             return false;
         case RGB_VAI:
-            if (rgb_matrix_get_val() >= (RGB_MATRIX_MAXIMUM_BRIGHTNESS - RGB_MATRIX_VAL_STEP)) {
-                indicator_enqueue(12, 200, 4, RGB_RED);   // . - VAI
-                indicator_enqueue(11, 200, 2, RGB_BLACK); // , - VAD
-                blink_space(false);
-                blink_arrows();
+            if (record->event.pressed) {
+                if (rgb_matrix_get_val() >= (RGB_MATRIX_MAXIMUM_BRIGHTNESS - RGB_MATRIX_VAL_STEP)) {
+                    indicator_enqueue(12, 200, 4, INDICATOR_RGB_DARK_RED); // . - VAI
+                    indicator_enqueue(11, 200, 2, RGB_BLACK);              // , - VAD
+                    blink_space(false);
+                    blink_arrows();
+                }
+                rgb_matrix_increase_val_noeeprom();
+            } else {
+                indicator_enqueue(12, 150, 1, RGB_WHITE); // . - VAI
             }
-            rgb_matrix_increase_val_noeeprom();
             return false;
         case RGB_VAD:
-            if (rgb_matrix_get_val() <= RGB_MATRIX_VAL_STEP) {
-                indicator_enqueue(12, 200, 2, RGB_BLACK); // . - VAI
-                indicator_enqueue(11, 200, 4, RGB_RED);   // , - VAD
-                blink_space(false);
-                blink_arrows();
+            if (record->event.pressed) {
+                if (rgb_matrix_get_val() <= RGB_MATRIX_VAL_STEP) {
+                    indicator_enqueue(12, 200, 2, RGB_BLACK);              // . - VAI
+                    indicator_enqueue(11, 200, 4, INDICATOR_RGB_DARK_RED); // , - VAD
+                    blink_space(false);
+                    blink_arrows();
+                }
+                rgb_matrix_decrease_val_noeeprom();
+            } else {
+                indicator_enqueue(11, 150, 1, RGB_WHITE); // , - VAD
             }
-            rgb_matrix_decrease_val_noeeprom();
             return false;
     }
     return true;
