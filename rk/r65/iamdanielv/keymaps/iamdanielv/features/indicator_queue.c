f56e2191a9fd0b2920895ac711185cadacd6391a
Daniel <1767914+iamdanielv@users.noreply.github.com>
2025-03-01 10:19:12 -0700

    indicatior_queue - Code cleanup

--- rk/r65/iamdanielv/keymaps/iamdanielv/features/indicator_queue.c
+++ rk/r65/iamdanielv/keymaps/iamdanielv/features/indicator_queue.c
@@ -1,10 +1,13 @@
 #include "indicator_queue.h"
 #include "indicators.h"
 
+#define TIMER_DEFAULT_VALUE 0x00
+
+// Enqueue an indicator to the queue
 void indicator_enqueue(uint8_t led_index, uint32_t interval, uint8_t times_to_flash, uint8_t r, uint8_t g, uint8_t b) {
     for (int i = 0; i < INDICATOR_QUEUE_MAX; i++) {
         if (!indicator_queue[i].active) {
-            // this queue position is not active, so we can use it
+            // This queue position is not active, so we can use it
             indicator_queue[i].active         = true;
             indicator_queue[i].led_index      = led_index;
             indicator_queue[i].last_update    = timer_read32();
@@ -13,40 +16,40 @@ void indicator_enqueue(uint8_t led_index, uint32_t interval, uint8_t times_to_fl
             indicator_queue[i].r              = r;
             indicator_queue[i].g              = g;
             indicator_queue[i].b              = b;
-            break;
+            return;
         }
     }
 }
 
+// Dequeue an indicator from the queue
 void indicator_dequeue(uint8_t led_index) {
     for (int i = 0; i < INDICATOR_QUEUE_MAX; i++) {
         if (indicator_queue[i].active && indicator_queue[i].led_index == led_index) {
-            // we found a match, deactivate it
+            // We found a match, deactivate it
             indicator_queue[i].active      = false;
-            indicator_queue[i].last_update = 0x00;
-            // break; // we could break out here, but check all of them in case we have duplicates
+            indicator_queue[i].last_update = TIMER_DEFAULT_VALUE;
+            // Continue checking in case there are duplicates
         }
     }
 }
 
+// Process the indicator queue
 void process_indicator_queue(uint8_t led_min, uint8_t led_max) {
     for (int i = 0; i < INDICATOR_QUEUE_MAX; i++) {
         if (indicator_queue[i].active) {
-            // this queue position is active, process it
+            // This queue position is active, process it
             if (timer_elapsed32(indicator_queue[i].last_update) >= indicator_queue[i].interval) {
-                // the timer has elapsed, perform the action
-
-                indicator_queue[i].last_update = timer_read32(); // reset the timer to now
+                // The timer has elapsed, perform the action
+                indicator_queue[i].last_update = timer_read32(); // Reset the timer to now
 
                 if (indicator_queue[i].times_to_flash) {
                     indicator_queue[i].times_to_flash--;
                 }
 
                 if (indicator_queue[i].times_to_flash <= 0) {
-                    // we have flashed as many times as requested
-                    // clear this queue spot
+                    // We have flashed as many times as requested, clear this queue spot
                     indicator_queue[i].active      = false;
-                    indicator_queue[i].last_update = 0x00;
+                    indicator_queue[i].last_update = TIMER_DEFAULT_VALUE;
                 }
             }
 
--- rk/r65/iamdanielv/keymaps/iamdanielv/features/indicator_queue.h
+++ rk/r65/iamdanielv/keymaps/iamdanielv/features/indicator_queue.h
@@ -2,21 +2,22 @@
 
 #include QMK_KEYBOARD_H
 
-// this is similar to RGB_MATRIX_INDICATOR_SET_COLOR
-// but takes in an indicator_t instead
+// This macro sets the color of an indicator if its led_index is within the valid range
 #define INDICATOR_Q_MATRIX_SET_COLOR(indicator)                                           \
-    if (indicator.led_index >= led_min && indicator.led_index < led_max) {                \
-        rgb_matrix_set_color(indicator.led_index, indicator.r, indicator.g, indicator.b); \
+    if ((indicator).led_index >= led_min && (indicator).led_index < led_max) {            \
+        rgb_matrix_set_color((indicator).led_index, (indicator).r, (indicator).g, (indicator).b); \
     }
 
+// This macro sets a custom color for an indicator if its led_index is within the valid range
 #define INDICATOR_Q_MATRIX_SET_COLOR_CUSTOM(indicator, r, g, b)            \
-    if (indicator.led_index >= led_min && indicator.led_index < led_max) { \
-        rgb_matrix_set_color(indicator.led_index, r, g, b);                \
+    if ((indicator).led_index >= led_min && (indicator).led_index < led_max) { \
+        rgb_matrix_set_color((indicator).led_index, (r), (g), (b));                \
     }
 
+// This macro retrieves the RGB values of an indicator as an rgb_t struct
 #define INDICATOR_Q_GET_RGB_LED(indicator)                   \
     (rgb_t) {                                            \
-        .r = indicator.r, .g = indicator.g, .b = indicator.b \
+        .r = (indicator).r, .g = (indicator).g, .b = (indicator).b \
     }
 
 #define INDICATOR_QUEUE_MAX 20
@@ -32,8 +33,25 @@ typedef struct {
     uint8_t  b;
 } indicator_t;
 
+// Indicator queue to hold the indicators
 indicator_t indicator_queue[INDICATOR_QUEUE_MAX];
 
+/**
+ * @brief Enqueue an indicator to the indicator queue.
+ *
+ * @param led_index Index of the LED to control.
+ * @param interval Time interval between flashes.
+ * @param times_to_flash Number of times to flash the LED.
+ * @param r Red color value.
+ * @param g Green color value.
+ * @param b Blue color value.
+ */
 void indicator_enqueue(uint8_t led_index, uint32_t interval, uint8_t times_to_flash, uint8_t r, uint8_t g, uint8_t b);
 
+/**
+ * @brief Process the indicator queue and update the LEDs.
+ *
+ * @param led_min Minimum LED index to process.
+ * @param led_max Maximum LED index to process.
+ */
 void process_indicator_queue(uint8_t led_min, uint8_t led_max);
